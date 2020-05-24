993cb8510579ea2a8b22de04773ce041029bbf99
Alex Dowdney <alexdowdney@gmail.com>
2020-05-23 21:18:10 -0400

    Add files via upload

---
+++ todolist/app.js
@@ -0,0 +1,55 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+// Event Listener
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+
+// Functions
+
+function addTodo(event){
+    event.preventDefault();
+    
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //DELETE BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    //DELETE TODO
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    if(item.classList[0] === "complete.btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
